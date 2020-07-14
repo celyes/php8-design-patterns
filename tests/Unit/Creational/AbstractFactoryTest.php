bfa9f1f35c0da20fc67780715e2ebc82b873a60b
celyes <celyes02@gmail.com>
2020-07-14 14:49:53 +0100

    simplify abstract factory pattern

--- tests/Unit/Creational/AbstractFactoryTest.php
+++ tests/Unit/Creational/AbstractFactoryTest.php
@@ -5,8 +5,6 @@ namespace Tests\Unit\Creational;
 use PHPUnit\Framework\TestCase;
 use Patterns\Creational\AbstractFactory\MercedesFactory;
 use Patterns\Creational\AbstractFactory\Models\MercedesModel;
-use Patterns\Creational\AbstractFactory\BMWFactory;
-use Patterns\Creational\AbstractFactory\Models\BMWModel;
 
 final class AbstractFactoryTests extends TestCase {
 
@@ -17,24 +15,10 @@ final class AbstractFactoryTests extends TestCase {
         $model = $mercedes->makeModel();
         $this->assertInstanceOf(MercedesModel::class, $model);
     }
-
-    public function testCanCreateBMWModel()
-    {
-        $mercedes = new BMWFactory();
-        $model = $mercedes->makeModel();
-        $this->assertInstanceOf(BMWModel::class, $model);
-    }
-
     public function testCanGetMercedesModelDescription()
     {
         $mercedes = new MercedesFactory();
         $model = $mercedes->makeModel();
         $this->assertEquals('A45S AMG', $model->getDescription());
-    }
-    public function testCanGetBMWModelDescription()
-    {
-        $bmw = new BMWFactory();
-        $model = $bmw->makeModel();
-        $this->assertEquals('M5 Competition', $model->getDescription());
     }
 }
